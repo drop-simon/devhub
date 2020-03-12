312a9098ffb761d3a2779452be4eefaf552a2062
drop-simon <simon@earnwithdrop.com>
2020-03-12 13:15:40 -0400

    remove some examples from App.tsx

--- App.tsx
+++ App.tsx
@@ -4,14 +4,10 @@ import MultipleInterpolationExample from './src/components/MultipleInterpolation
 import ConfettiVisualization from './src/components/ConfettiVisualization'
 import ConfettiExample from './src/components/ConfettiExample'
 import ConfettiFinished from './src/components/ConfettiExample/ConfettiFinished'
-import CombiningExample from './src/components/CombiningExample'
-import ScrollExample from './src/components/ScrollExample'
 
 export default function App() {
   return <ColorExample />
   return <MultipleInterpolationExample />
-  return <CombiningExample />
-  return <ScrollExample />
   return <ConfettiFinished />
   return <ConfettiVisualization />
   return <ConfettiExample />
